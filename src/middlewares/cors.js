ff8ce960d13c9dfbcf99ca0fe0dd357cf42d7fc3
programador16 <programador16@tecnologicocomfenalco.edu.co>
2024-09-13 14:51:56 -0500

    23

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,14 +1,6 @@
 import cors from "cors";
 
-const ACCEPTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://localhost:3000',
-    'http://localhost:5000',
-    'https://api-node-zpxl.onrender.com',
-    'https://api-node-aiwe.netlify.app',
-    'https://elchocho.netlify.app',
-    'https://e-commerse-luru.onrender.com'
-  ]
+const ACCEPTED_ORIGINS = "*";
 
 export const corsMiddleware = ({acceptedOrigins = ACCEPTED_ORIGINS} = {} ) => cors({
     origin: (origin, callback) => {      
